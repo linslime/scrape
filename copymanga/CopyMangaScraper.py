9d6b0f968716233084ba4f6d5e8c675a12ec61a5
linslime <linslime@foxmail.com>
2024-02-20 14:49:20 +0800

    爬取拷贝漫画--代码重构

--- copymanga/CopyMangaScraper.py
+++ copymanga/CopyMangaScraper.py
@@ -11,6 +11,72 @@ from selenium.webdriver.support.ui import WebDriverWait
 from concurrent.futures import ThreadPoolExecutor
 import img2pdf
 import threading
+from PyPDF2.generic import RectangleObject
+from PyPDF2 import PdfReader, PdfWriter, Transformation, PdfMerger
+from multiprocessing import Process, Pool
+
+
+def get_name(num):
+    if num < 10:
+        return '00' + str(num)
+    elif num < 100:
+        return '0' + str(num)
+    else:
+        return str(num)
+
+def create_small_pdf(i, images_dir, name):
+    path_gap = './comics/' + name + '/pdf_gap/' + get_name(i) + '.pdf'
+    # 创建一个PDF文件 并以二进制方式写入
+    with open(path_gap, "wb") as file:
+        # convert函数 用来转PDF
+        write_content = img2pdf.convert(images_dir[i])
+        file.write(write_content)  # 写入文件
+    file.close()
+
+    reader = PdfReader(path_gap)
+    writer = PdfWriter()
+    index = 0
+    total_page = reader.pages[index]
+    while True:
+        index += 1
+        if index < len(reader.pages):
+            if total_page.cropbox.width == reader.pages[index].cropbox.width:
+                page = reader.pages[index]
+                total_page = merge_pdf_bottom(total_page, page)
+            else:
+                writer.add_page(total_page)
+                total_page = reader.pages[index]
+        else:
+            writer.add_page(total_page)
+            break
+
+    path = './comics/' + name + '/pdf/' + get_name(i) + '.pdf'
+
+    with open(path, "wb") as file:
+        writer.write(file)
+    file.close()
+
+def merge_pdf_bottom(page2, page1):
+        height = page1.cropbox.height
+        op = Transformation().translate(tx=0, ty=height)  # 拼接在下方
+        page2.add_transformation(op)
+        cb = page2.cropbox
+
+        page2.mediabox = RectangleObject((cb.left, cb.bottom + height, cb.right, cb.top + height))
+        page2.cropbox = RectangleObject((cb.left, cb.bottom + height, cb.right, cb.top + height))
+        page2.trimbox = RectangleObject((cb.left, cb.bottom + height, cb.right, cb.top + height))
+        page2.bleedbox = RectangleObject((cb.left, cb.bottom + height, cb.right, cb.top + height))
+        page2.artbox = RectangleObject((cb.left, cb.bottom + height, cb.right, cb.top + height))
+
+        page1.merge_page(page2, expand=True)
+        mb = page1.mediabox
+        page1.mediabox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+        page1.cropbox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+        page1.trimbox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+        page1.bleedbox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+        page1.artbox = RectangleObject((mb.left, mb.bottom, mb.right, mb.top))
+
+        return page1
 
 class MangaScraper(object):
     def __init__(self,url):
@@ -35,15 +101,12 @@ class MangaScraper(object):
         self.get_url_index()
         print(self.name + ',漫画目录下载完成')
 
-        print('创建线程池')
-        pool = ThreadPoolExecutor(max_workers=60)
-        print('线程池创建完成')
-
         print('开始初始化图片url')
         self.init_images_url()
         print('完成图片url初始化')
 
         print('开始下载图片url')
+        thread_pool = ThreadPoolExecutor(max_workers=60)
         num = 0
         while not self.check_url_index():
             if num > 0:
@@ -52,14 +115,14 @@ class MangaScraper(object):
             futures = []
             for i in range(len(self.url_index)):
                 if len(self.images_url[i]) == 0:
-                    futures.append(pool.submit(lambda cxp: self.get_images_url(*cxp), (self.website + self.url_index[i], i)))
+                    futures.append(thread_pool.submit(lambda cxp: self.get_images_url(*cxp), (self.website + self.url_index[i], i)))
             concurrent.futures.wait(futures)
             print('开始第' + str(num) + '轮检查')
         print('通过第' + str(num) + '轮检查，图片url下载完成')
 
-        print('开始创建目录')
-        self.create_comics_dir()
-        print('目录创建成功')
+        print('开始创建图片目录')
+        self.create_images_dir()
+        print('图片目录创建成功')
 
         print('开始检测图片是否存在')
         self.init_images_exist()
@@ -75,14 +138,31 @@ class MangaScraper(object):
             for i in range(len(self.images_url)):
                 for j in range(len(self.images_url[i])):
                     if not self.images_exist[i][j]:
-                        futures.append(pool.submit(lambda cxp: self.save_image(*cxp), (self.images_url[i][j], i, j)))
+                        futures.append(thread_pool.submit(lambda cxp: self.save_image(*cxp), (self.images_url[i][j], i, j)))
             concurrent.futures.wait(futures)
             print('开始第' + str(num) + '轮检查')
         print('通过第' + str(num) + '轮检查，图片下载完成')
 
-        print('开始创建pdf')
-        self.create_pdf()
-        print('pdf创建成功')
+        print('获取图片目录')
+        base_dir = './comics/' + self.name + '/images/'
+        # 获取当前目录下的所有文件
+        files = os.listdir(base_dir)
+        self.images_dir = [[base_dir + filename + '/' + image_name for image_name in os.listdir(base_dir + filename)] for filename in files]
+        path = './comics/' + self.name + '/pdf'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path = './comics/' + self.name + '/pdf_gap'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        print('图片目录获取成功')
+
+        # futures = []
+        # for i in range(len(self.images_dir)):
+        #     futures.append(thread_pool.submit(self.create_small_pdf, i))
+        # concurrent.futures.wait(futures)
+
+
+
 
     def init_images_url(self):
         path = self.address + '/' + self.name + '/' + self.name + '.csv'
@@ -105,7 +185,7 @@ class MangaScraper(object):
                 if os.path.exists(path):
                     self.images_exist[row][col] = 1
 
-    def create_comics_dir(self):
+    def create_images_dir(self):
         for i in range(len(self.url_index)):
             path = self.address + '/' + self.name + '/images/' + self.get_name(i)
             if not os.path.exists(path):
@@ -125,22 +205,37 @@ class MangaScraper(object):
         csv.writer(file).writerows(self.images_url)
         file.close()
 
-    def create_pdf(self):
-        # 当前目录
-        base_dir = './comics/' + self.name + '/images/'
-        # 获取当前目录下的所有文件
-        files = os.listdir(base_dir)
-        images_dir = []
-        for filename in files:
-            images_dir += [base_dir + filename + '/' + image_name for image_name in os.listdir(base_dir + filename)]
-        self.images_dir = images_dir
-
-        output = './comics/' + self.name + '/' + self.name + '.pdf'
+    def create_small_pdf(self, i):
+        path_gap = './comics/' +self.name + '/pdf_gap/' + self.get_name(i) + '.pdf'
         # 创建一个PDF文件 并以二进制方式写入
-        with open(output, "wb") as f:
+        with open(path_gap, "wb") as file:
             # convert函数 用来转PDF
-            write_content = img2pdf.convert(self.images_dir)
-            f.write(write_content)  # 写入文件
+            write_content = img2pdf.convert(self.images_dir[i])
+            file.write(write_content)  # 写入文件
+        file.close()
+
+        reader = PdfReader(path_gap)
+        writer = PdfWriter()
+        index = 0
+        total_page = reader.pages[index]
+        while True:
+            index += 1
+            if index < len(reader.pages):
+                if total_page.cropbox.width == reader.pages[index].cropbox.width:
+                    page = reader.pages[index]
+                    total_page = self.merge_pdf_bottom(total_page, page)
+                else:
+                    writer.add_page(total_page)
+                    total_page = reader.pages[index]
+            else:
+                writer.add_page(total_page)
+                break
+
+        path = './comics/' + self.name + '/pdf/' + self.get_name(i) + '.pdf'
+
+        with open(path, "wb") as file:
+            writer.write(file)
+        file.close()
 
     def get_name(self, num):
         if num < 10:
@@ -197,5 +292,24 @@ class MangaScraper(object):
         return True
 
 if __name__ == '__main__':
-    scraper = MangaScraper('https://www.copymanga.site/comic/eyizhiyousiwangjieju')
+    scraper = MangaScraper('https://www.copymanga.site/comic/mswgtqlysjsjhgbjbsds')
     scraper.start()
+
+    print('开始创建章节pdf')
+    pool = Pool(10)
+    results = []
+    for i in range(len(scraper.images_dir)):
+        pool.apply_async(func=create_small_pdf, args=(i,scraper.images_dir,scraper.name))
+        # print(i)
+    # 等待所有任务完成
+    pool.close()
+    pool.join()
+    print('完成章节pdf创建')
+
+    print('开始创建总pdf')
+    file_merger = PdfMerger()
+    paths = ['./comics/' + scraper.name + '/pdf/' + scraper.get_name(i) + '.pdf' for i in range(len(scraper.images_url))]
+    for pdf in paths:
+        file_merger.append(pdf)
+    file_merger.write('./comics/' + scraper.name + '/' + scraper.name + '.pdf')
+    print('总pdf创建成功')
---
+++ copymanga/MainProcess.py
@@ -0,0 +1,5 @@
+from CopyMangaScraper import *
+from PDFManagement import *
+from FileDirectoryManagement import *
+
+if __name__ == '__main__':
