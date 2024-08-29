618f4548ed80ff3d12785d1235780b01e2bfe47b
linslime <linslime@foxmail.com>
2024-08-29 21:03:13 +0800

    删除没有使用的包 创建得到浏览器工具的函数 创建获得m3u8网址的方法

--- gugufan/MainProcess.py
+++ gugufan/MainProcess.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from parsel import Selector
 import re
 
@@ -15,6 +12,24 @@ def get_homepage_website(code):
     return "https://www.gugufan.org/index.php/vod/detail/id/" + str(code) + ".html"
 
 
+def get_browser(type):
+    """
+    返回指定浏览器类型的工具
+    :param type: 浏览器类型
+    :return: 浏览器工具
+    """
+    if type == "chrome":
+        browser_options = webdriver.ChromeOptions()
+        browser_options.add_argument("headless")
+        browser = webdriver.Chrome(options=browser_options)
+        return browser
+    elif type == "edge":
+        browser_options = webdriver.EdgeOptions()
+        browser_options.add_argument("headless")
+        browser = webdriver.Edge(options=browser_options)
+        return browser
+
+
 def get_name_and_episodes(url):
     """
     通过番剧主页地址，获得番剧的名字和最新集数
@@ -23,9 +38,7 @@ def get_name_and_episodes(url):
     cartoon_name:番剧的名字
     episode:番剧的最新集数
     """
-    browser_options = webdriver.ChromeOptions()
-    browser_options.add_argument("headless")
-    browser = webdriver.Chrome(options=browser_options)
+    browser = get_browser('edge')
     browser.get(url)
     selector = Selector(browser.page_source)
     cartoon_name = selector.css('.slide-info-title *::text').get()
@@ -47,9 +60,22 @@ def get_episode_website(code, episode):
     return websites
 
 
+def get_m3u8_url(url):
+    """
+    通过一集番剧的网址，得到.m3u8文件的网址
+    :param url:
+    :return:
+    """
+    browser = get_browser('edge')
+    browser.get(url)
+    m3u8_url = re.search(pattern="url_next\":\"(.*?)\"", string=browser.page_source).group(1)
+    return m3u8_url
+
+
 if __name__ == "__main__":
     code = 2738
     homepage_website = get_homepage_website(code)
     cartoon_name, episode = get_name_and_episodes(homepage_website)
     websites = get_episode_website(episode, code)
+    m3u8_url = get_m3u8_url(websites[0])
     print(homepage_website)
