9283c3039ca7a8d02bd43c52081099785c5eb7c2
linslime <linslime@foxmail.com>
2024-09-05 17:02:47 +0800

    多路复用

--- miniredis/miniredis.py
+++ miniredis/miniredis.py
@@ -11,14 +11,40 @@ def start_server_socket():
     :return:
     """
     ip, port = Config.SOCKET_IP
-    server = socket.socket(type=socket.SOCK_DGRAM)  # 使用UDP方式传输
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 使用UDP方式传输
     server.bind((ip, port))  # 绑定IP与端口
-
+    server.listen(200)
+    # 将 server_socket 设置为非阻塞模式
+    server.setblocking(False)
+    # 存储已连接的 client_socket
+    connected_clients = []
     # 不断循环，接受客户端发来的消息
     while True:
-        socket_data, address = server.recvfrom(Config.BUFFER_SIZE)
-        result = socket_data
-        server.sendto(result, address)
+        # 使用 select 函数监听所有连接的 client_socket
+        readable_sockets, _, _ = select.select([server] + connected_clients, [], [])
+        # 处理所有可读的 socket
+        for sock in readable_sockets:
+            # 如果是 server_socket 表示有新的连接
+            if sock is server:
+                client_socket, client_address = server.accept()
+                connected_clients.append(client_socket)
+                # 否则是已连接的 client_socket，需要处理收到的数据
+            else:
+                try:
+                    socket_data = sock.recv(Config.BUFFER_SIZE)
+                    if socket_data:
+                        # print(socket_data)
+                        result = process_data(socket_data)
+                        sock.send(result)
+                    else:
+                        sock.close()
+                        connected_clients.remove(sock)
+                        print("Connection closed")
+                except Exception as e:
+                    # 出现异常，也需要关闭 socket 并从 connected_clients 中移除
+                    print(f"Error occurred while receiving data from {sock.getpeername()}: {e}")
+                    sock.close()
+                    connected_clients.remove(sock)
 
 
 def start_client_socket():
@@ -27,31 +53,34 @@ def start_client_socket():
     :return:
     """
     ip, port = Config.SOCKET_IP
-    client = socket.socket(type=socket.SOCK_DGRAM)  # 使用TCP方式传输
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 使用TCP方式传输
     client.connect((ip, port))  # 连接远程服务端
     data = [i for i in range(10000)]
     data = pickle.dumps(data)
     # 与服务端交互
-    for i in range(10000):
+    for i in range(1000):
         client.sendto(data, Config.SOCKET_IP)  # 使用sendto发送UDP消息，address填入服务端IP和端口
-        socket_data, address = client.recvfrom(Config.BUFFER_SIZE)
+        socket_data = client.recv(Config.BUFFER_SIZE)
         if socket_data != data:
             print("出错了")
+    client.close()
     print("success")
 
 
-if __name__ == '__main__':
-    processes = list()
+def process_data(data):
+    return data
 
+
+if __name__ == '__main__':
     process = multiprocessing.Process(target=start_server_socket, args=())
-    # processes.append(process)
     process.start()
 
-    for i in range(1):
+    processes = list()
+    for i in range(100):
         process = multiprocessing.Process(target=start_client_socket, args=())
         processes.append(process)
+    for process in processes:
         process.start()
 
     for process in processes:
         process.join()
-    # start_server_socket()
